ff6ead0eca512755b5e2a95d3d5707d5cbd77a7f
Icaro Oliveira <icarovinici@gmail.com>
2021-03-17 16:50:08 -0300

    refactor: add default data

--- src/services/fakeServer.ts
+++ src/services/fakeServer.ts
@@ -1,9 +1,32 @@
 import { createServer, Model } from 'miragejs'
 
+const fakeData = [
+  {
+    id: 1,
+    title: 'Freelance de website',
+    type: 'deposit',
+    category: 'Dev',
+    amount: 6000,
+    createdAt: new Date('2021-02-12 09:00:00'),
+  },
+  {
+    id: 2,
+    title: 'Aluguel',
+    type: 'withdraw',
+    category: 'Casa',
+    amount: 1200,
+    createdAt: new Date('2021-02-14 12:00:00'),
+  },
+]
+
 export function server() {
   createServer({
     models: {
       transaction: Model,
+    },
+
+    seeds(server) {
+      server.db.loadData({ transactions: fakeData })
     },
 
     routes() {
